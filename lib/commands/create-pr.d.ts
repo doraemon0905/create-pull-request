ea8322e9c38a0bb12b678712fb76ccc9afaa0d51
Doraemon <bradley.nguyen@employmenthero.com>
2025-09-21 14:37:23 +0700

    Clean cache

--- lib/commands/create-pr.d.ts
+++
@@ -1,9 +0,0 @@
-export interface CreatePROptions {
-    jira?: string;
-    base?: string;
-    title?: string;
-    dryRun?: boolean;
-    draft?: boolean;
-}
-export declare function createPullRequest(options: CreatePROptions): Promise<void>;
-//# sourceMappingURL=create-pr.d.ts.map
